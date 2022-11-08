ad4b73d54e8510919fc3b34ce82ad86c534342f6
Aox <92138800+Ssspil@users.noreply.github.com>
2022-11-08 19:34:00 +0900

    관리자페이지 (공지)
    관리자페이지 (공지) -정승필

---
+++ MeisterProject/src/com/kh/manager/Controller/NoticeManagerController.java
@@ -0,0 +1,52 @@
+package com.kh.manager.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class NoticeManagerController
+ */
+@WebServlet("/notice.ad")
+public class NoticeManagerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeManagerController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	    // 관리자가 아니면 실행 안되게 하는 것.
+	    if( !(request.getSession().getAttribute("loginUser") != null && 
+	            ((Member)request.getSession().getAttribute("loginUser")).getUserId().equals("admin@admin"))) {
+	        request.setAttribute("errorMsg", "관리자 권한이 없습니다.");
+	        request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+	        
+	        return;
+	    }
+	    
+		request.getRequestDispatcher("views/manager/noticeManager.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
