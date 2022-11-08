6ce519859e6a62c92d1c805c75f746803f41dd9b
woongz1 <qnvudwkdl@naver.com>
2022-11-08 19:53:24 +0900

    2022 11 08
    개인정보 수정 페이지, 전문가 인증 페이지 제작

---
+++ MeisterProject/src/com/kh/member/controller/MemberUpdateController.java
@@ -0,0 +1,54 @@
+package com.kh.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.member.model.service.MemberService;
+import com.kh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class MemberUpdateController
+ */
+@WebServlet("/update.me")
+public class MemberUpdateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberUpdateController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String userId = request.getParameter("userId");
+		String nickName = request.getParameter("nickname");
+		String interest = request.getParameter("interest");
+		String userName = request.getParameter("userName");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		
+		Member m  = new Member(userId, nickName, interest, userName, phone, email);
+		
+		Member updateMem = new MemberService().updateMember(m);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
