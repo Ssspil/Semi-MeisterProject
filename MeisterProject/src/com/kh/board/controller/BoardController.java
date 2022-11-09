17bbda25df48a8d2a122a9d41101ced22357ff4f
yisoo98776 <116542266+yisoo98776@users.noreply.github.com>
2022-11-09 16:53:19 +0900

    보드컨트롤러

--- MeisterProject/src/com/kh/board/controller/BoardController.java
+++ MeisterProject/src/com/kh/board/controller/BoardController.java
@@ -1,11 +1,18 @@
 package com.kh.board.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.board.model.service.BoardService;
+import com.kh.board.model.vo.Attachment;
+import com.kh.board.model.vo.Board;
+import com.kh.common.model.vo.PageInfo;
 
 /**
  * Servlet implementation class BoardController
@@ -26,7 +33,9 @@ public class BoardController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.getRequestDispatcher("views/board/boardMainPage.jsp").forward(request, response);
+		
 	}
 
 	/**
