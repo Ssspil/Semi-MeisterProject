6a990640df560796b0ed1bb29f3a47475999bc05
woongz1 <qnvudwkdl@naver.com>
2022-11-15 21:50:25 +0900

    2022 11 15
    쪽지 리스트, 상세보기

--- MeisterProject/src/com/kh/chatting/model/service/ChattingService.java
+++ MeisterProject/src/com/kh/chatting/model/service/ChattingService.java
@@ -1,8 +1,10 @@
 package com.kh.chatting.model.service;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import com.kh.chatting.model.dao.ChattingDao;
+import com.kh.chatting.model.vo.Chatting;
 import com.kh.common.JDBCTemplate;
 
 public class ChattingService {
@@ -21,5 +23,21 @@ public class ChattingService {
 	      JDBCTemplate.close();
 	      
 	      return result;
+	}
+	
+	public ArrayList<Chatting> selectNoteList(int userNo){
+		Connection conn = JDBCTemplate.getConnection();
+		ArrayList<Chatting> list = new ChattingDao().selectNoteList(conn, userNo);
+		
+		JDBCTemplate.close();
+		return list;
+	}
+	
+	public ArrayList<Chatting> selectNote(int sender, int receiver, int sellNo){
+		Connection conn = JDBCTemplate.getConnection();
+		ArrayList<Chatting> list = new ChattingDao().selectNote(conn, sender, receiver, sellNo);
+		
+		JDBCTemplate.close();
+		return list;
 	}
 }
