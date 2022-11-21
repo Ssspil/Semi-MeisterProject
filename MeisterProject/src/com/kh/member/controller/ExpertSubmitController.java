72fc86c09aae5518b0e657a718ab52fb2382ef7a
woongz1 <47379227+woongz1@users.noreply.github.com>
2022-11-21 15:56:21 +0900

    2022 11 21
    전문가 인증 첨부 파일 추가

--- MeisterProject/src/com/kh/member/controller/ExpertSubmitController.java
+++ MeisterProject/src/com/kh/member/controller/ExpertSubmitController.java
@@ -8,8 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.kh.common.MyFileRenamePolicy;
+import com.kh.common.model.vo.Attachment;
 import com.kh.member.model.service.MemberService;
 import com.kh.member.model.vo.Member;
+import com.oreilly.servlet.MultipartRequest;
 
 /**
  * Servlet implementation class ExpertSubmitController
@@ -31,29 +36,46 @@ public class ExpertSubmitController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
-		String userId = request.getParameter("userId");
-		String userName = request.getParameter("userName");
-		String gender = request.getParameter("gender");
-		System.out.println(request.getParameter("phoneMid"));
-		System.out.println(request.getParameter("phoneLast"));
-		String phone = ("010".concat(request.getParameter("phoneMid"))).concat(request.getParameter("phoneLast"));
-		String email = ((request.getParameter("emailFront")).concat("@")).concat(request.getParameter("emailBack"));
-		String speciality = request.getParameter("speciality");
+		if(ServletFileUpload.isMultipartContent(request)) {
+			int maxSize = 1024 * 1024 * 10;
 	
-		Member m = new Member(userId, userName, gender, email, phone, speciality, "Y");
-
-		Member result = new MemberService().expertSubmit(m);
-		
-		if(result == null) {
-			request.setAttribute("errorMsg", "회원정보 수정에 실패했습니다.");
-			request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
-		} else {
-			HttpSession session = request.getSession();
-			session.setAttribute("loginUser", result);
-			session.setAttribute("alertMsg", "제출");
-
-			response.sendRedirect(request.getContextPath()+"/mypage.me");
+			String savePath = request.getSession().getServletContext().getRealPath("/resources/expert_upfiles/");
+				
+			MultipartRequest multiRequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+	
+			int userNo = Integer.parseInt(multiRequest.getParameter("userNo"));
+			String userId = multiRequest.getParameter("userId");
+			String userName = multiRequest.getParameter("userName");
+			String gender = multiRequest.getParameter("gender");
+			String phone = ("010".concat(multiRequest.getParameter("phoneMid"))).concat(multiRequest.getParameter("phoneLast"));
+			String email = ((multiRequest.getParameter("emailFront")).concat("@")).concat(multiRequest.getParameter("emailBack"));
+			String speciality = multiRequest.getParameter("speciality");
+			System.out.println(userNo);
+			System.out.println(multiRequest.getOriginalFileName("expertFile"));
+			Attachment at = null;
+			
+			if(multiRequest.getOriginalFileName("expertFile") != null) {	
+				at = new Attachment();
+				at.setRefNo(userNo);
+				at.setOriginName(multiRequest.getOriginalFileName("expertFile"));
+				at.setChangeName(multiRequest.getFilesystemName("expertFile"));
+				at.setFilePath("resources/expert_upfiles/");
+			}
+			
+			Member m = new Member(userId, userName, gender, email, phone, speciality, "Y");
+	
+			Member result = new MemberService().expertSubmit(m, at);
+			
+			if(result == null) {
+				request.setAttribute("errorMsg", "회원정보 수정에 실패했습니다.");
+				request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+			} else {
+				HttpSession session = request.getSession();
+				session.setAttribute("loginUser", result);
+				session.setAttribute("alertMsg", "제출");
+	
+				response.sendRedirect(request.getContextPath()+"/mypage.me");
+			}
 		}
 	}
 
