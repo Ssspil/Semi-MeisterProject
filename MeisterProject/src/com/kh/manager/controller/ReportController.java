5d2169f6bd7c5b853816cfb2cb5d35993c4a47a5
Aox <92138800+Ssspil@users.noreply.github.com>
2022-11-22 19:57:21 +0900

    신고하기 완료

--- MeisterProject/src/com/kh/manager/controller/ReportController.java
+++ MeisterProject/src/com/kh/manager/controller/ReportController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.common.model.vo.PageInfo;
+import com.kh.member.model.vo.Member;
 import com.kh.report.model.service.ReportService;
 import com.kh.report.model.vo.Report;
 
@@ -32,9 +34,62 @@ public class ReportController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		ArrayList<Report> list = new ReportService().selectAllList();
+		System.out.println("신고관리 페이지로 이동");
+		
+	    // 관리자가 아니면 실행 안되게 하는 것.
+	    if( !(request.getSession().getAttribute("loginUser") != null && 
+	            ((Member)request.getSession().getAttribute("loginUser")).getUserId().equals("admin@admin.com"))) {
+	        request.setAttribute("errorMsg", "관리자 권한이 없습니다.");
+	        request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+	        
+	        return;
+	    }
+	    
+	 // --------- 페이징처리 시작 -------- 
+	    int listCount; //현재 총 게시글 갯수
+		int currentPage; //현재 페이지(사용자가 요청한 페이지)
+		int pageLimit; //페이지 하단에 보여질 페이징바의 페이지 최대 갯수
+		int boardLimit; // 페이지에 보여질 게시글의 최대 갯수
+		
+		int maxPage; //가장 마지막 페이지가 몇번째 페이지인지 나타냄(총 페이지 수)
+		int startPage;// 페이지 하단에 보여질 페이징바의 시작수
+		int endPage;
+	    
+		
+		
+		listCount = new ReportService().selectReportCount(); 
+		
+		// * currentPage : 현재페이지
+		currentPage = Integer.parseInt(request.getParameter("currentPage") == null ? "1" : request.getParameter("currentPage"));
+		
+		// * pageLimit : 페이지 하단에 보여질 페이징바의 페이지 최대 갯수(페이지목록들을 몇 개 단위로 출력할건지!)
+		pageLimit = 10;
+		
+		// * boardLimit : 한 페이지에 보여질 게시글의 최대갯수(게시글 몇 개 단위씩)
+		boardLimit = 20;
+	    
+		maxPage = (int)Math.ceil((double)listCount/boardLimit);
+	    
+		startPage = (currentPage -1 ) /pageLimit * pageLimit + 1;
+	    
+		endPage  = startPage + pageLimit -1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		// 1. 페이징바 만들 때 필요한 객체
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+	    
+	    
+	    // --------- 페이징처리 끝 --------  //
+		
+
+		
+		ArrayList<Report> list = new ReportService().selectAllList(pi);
 		
 		request.setAttribute("list", list);
+		request.setAttribute("pi", pi);
 		
 		request.getRequestDispatcher("views/manager/reportManager.jsp").forward(request, response);
 		
--- MeisterProject/src/com/kh/report/model/dao/ReportDao.java
+++ MeisterProject/src/com/kh/report/model/dao/ReportDao.java
@@ -13,6 +13,7 @@ import java.util.Properties;
 
 import com.kh.common.JDBCTemplate;
 import com.kh.common.model.vo.Attachment;
+import com.kh.common.model.vo.PageInfo;
 import com.kh.member.model.dao.MemberDao;
 import com.kh.report.model.vo.Report;
 
@@ -45,17 +46,14 @@ public class ReportDao {
 		
 		String sql = prop.getProperty("insertReport");
 		
-		System.out.println("dao의 re.getReportUser() : " + re.getReportUser());
-		System.out.println("re.getReportUser() : " + re.getReportedUser());
-		
 		try {
 			psmt = conn.prepareStatement(sql);
 			
 			psmt.setString(1, re.getReportTitle());
 			psmt.setString(2, re.getReportContent());
 			psmt.setString(3, re.getReason());
-			psmt.setInt(4, re.getReportUser());
-			psmt.setInt(5, re.getReportedUser());
+			psmt.setInt(4, re.getReportUserNo());
+			psmt.setInt(5, re.getReportedUserNo());
 			
 			result1 = psmt.executeUpdate();
 			
@@ -99,7 +97,14 @@ public class ReportDao {
 	        
 	        return result2;
 	    }
-	public ArrayList<Report> selectAllList(Connection conn) {
+	
+	/**
+	 * 신고접수 전체 보기 (페이징처리 포함)
+	 * @param pi
+	 * @param conn
+	 * @return
+	 */
+	public ArrayList<Report> selectAllList(PageInfo pi, Connection conn) {
 		
 		ArrayList<Report> list = new ArrayList<>();
 		PreparedStatement psmt = null;
@@ -110,12 +115,94 @@ public class ReportDao {
 		try {
 			psmt = conn.prepareStatement(sql);
 			
+			int startRow = (pi.getCurrentPage()-1) * pi.getBoardLimit() +1;
+			int endRow = startRow + pi.getBoardLimit() -1;
+			
+			psmt.setInt(1, startRow);
+			psmt.setInt(2, endRow);
+			
+			rset = psmt.executeQuery();
+			
+			while(rset.next()) {
+				list.add(new Report(rset.getInt("REPORT_NO"),
+									rset.getString("REPORT_TITLE"),
+									rset.getString("REASON"),
+									rset.getString("REPORT_CONTENT"),
+									rset.getDate("REPORT_DATE"),
+									rset.getString("STATUS"),
+									rset.getString("REPORT_USER"),
+									rset.getString("REPORTED_USER")
+									));
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close();
 		}
 		
 		return list;
+	}
+	
+	/**
+	 * 신고접수글 전체 개수 
+	 * @param conn
+	 * @return
+	 */
+	public int selectReportCount(Connection conn) {
+		//select문 -> ResultSet객체
+		int listCount = 0;
+		
+		PreparedStatement psmt = null;
+		
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("selectReportCount");
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			
+			rset = psmt.executeQuery();
+			
+			if(rset.next()) {
+				listCount = rset.getInt("COUNT");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(psmt);
+		}
+		return listCount;
+	}
+	
+	/**
+	 * 처리완료하는 메소드
+	 * @param rpno
+	 * @return
+	 */
+	public int reportProcessed(int rpno, Connection conn) {
+		
+		int complete = 0;
+		
+		PreparedStatement psmt = null;
+		
+		String sql = prop.getProperty("reportProcessed");
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			
+			psmt.setInt(1, rpno);
+			
+			complete = psmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(psmt);
+		}
+		
+		return complete;
 	}
 
 
