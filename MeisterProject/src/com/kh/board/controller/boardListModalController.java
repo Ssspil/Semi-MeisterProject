10e6aaeb283e9c218a4717d32ecebf7f0b5f5e75
sang-3 <111002098+sang-3@users.noreply.github.com>
2022-11-29 21:37:11 +0900

    2022 11 29

--- MeisterProject/src/com/kh/board/controller/boardListModalController.java
+++ MeisterProject/src/com/kh/board/controller/boardListModalController.java
@@ -2,6 +2,7 @@ package com.kh.board.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,30 +35,50 @@ public class boardListModalController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
 		int boardNo = Integer.parseInt(request.getParameter("bno"));
+		
 		String type = request.getParameter("type") == null ? "" : request.getParameter("type");
 		
 		
+		
 		if("".equals(type)) {
-			ArrayList<Board> list = new BoardService().modalList();
-			request.setAttribute("list", list);
-			System.out.println(list);
+	
+			Attachment at = new BoardService().selectAttachment(boardNo);
+			request.setAttribute("at", at);
 			
 			ArrayList<Reply> rlist = new BoardService().replyList(boardNo);
 			request.setAttribute("rlist", rlist);
 			
-			request.getRequestDispatcher("views/manager/boardManagerModal.jsp").forward(request, response);
-		} else {
-			ArrayList<Reply> rlist = new BoardService().replyList(boardNo);
-			//request.setAttribute("rlist", rlist);
-			
-			System.out.println(rlist);
+			response.setContentType("application/json; charset=UTF-8");
 			Gson gson = new Gson();
 			response.getWriter().print(gson.toJson(rlist));
+			
+			//request.getRequestDispatcher("views/manager/boardManagerModal.jsp").forward(request, response);
+		} else {
+
+			Attachment at = new BoardService().selectAttachment(boardNo);
+			
+			ArrayList<Reply> rlist = new BoardService().replyList(boardNo);
+			
+			
+			response.setContentType("application/json; charset=UTF-8");
+			
+			HashMap<String, Object> b = new HashMap<>();
+			
+			Board bs = new BoardService().selectBoard(boardNo);
+			
+			b.put("at", at);
+			b.put("rlist", rlist);
+			b.put("bs", bs);
+			
+			Gson gson = new Gson();
+			response.getWriter().print(gson.toJson(b));
 		}
 			
 			
-			
+		
 		
 		
 	}
